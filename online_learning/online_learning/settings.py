580ed2d710baede2198934ae5d69d7caf7e9081b
rakhi chander <mamidirakhichaner@gmail.com>
2026-05-26 12:25:20 +0530

    Update settings.py

--- online_learning/online_learning/settings.py
+++ online_learning/online_learning/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
+    
     'accounts',
-    'progress',
-    'courses',
+    
+
 ]
 
 MIDDLEWARE = [
